abbdc421ff174c1b02049eff9fa93b19fc79d24d
colinbankier <colinbankier@gmail.com>
2013-11-26 15:26:09 +1000

    Add vagrant to ssh group so can ssh after puppet run.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,7 @@
 sudo groupmod -g 991 vboxsf
 sudo find /vagrant -gid 1000 -exec chgrp -h 991 '{}' \+
+sudo usermod -a -G ssh vagrant
+
 rm /etc/hiera.yaml
 ln -s /vagrant/puppet/hiera.yaml /etc/hiera.yaml
 
