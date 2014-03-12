f3c27861d6ddf15a74fa5ebae2486f1f50304ff1
colinbankier <colinbankier@gmail.com>
2014-03-12 17:41:31 +1000

    Install hiera-eyaml gem onto bax on bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,13 @@
-sudo groupmod -g 991 vboxsf
-sudo find /vagrant -gid 1000 -exec chgrp -h 991 '{}' \+
-sudo usermod -a -G ssh vagrant
+groupmod -g 991 vboxsf
+find /vagrant -gid 1000 -exec chgrp -h 991 '{}' \+
+usermod -a -G ssh vagrant
 
 rm /etc/hiera.yaml
 ln -s /vagrant/puppet/hiera.yaml /etc/hiera.yaml
 
 rm -rf /etc/puppet
 ln -s /vagrant/puppet /etc/puppet
+
+apt-get update
+apt-get install -y rubygems
+gem install hiera-eyaml
