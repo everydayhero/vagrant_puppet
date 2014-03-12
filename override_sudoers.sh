17843353c0adc7be557c92746b55e725e01a73cb
colinbankier <colinbankier@gmail.com>
2014-03-12 17:51:46 +1000

    Add hints to override_sudoers script.

--- override_sudoers.sh
+++ override_sudoers.sh
@@ -2,4 +2,5 @@ SSH_PORT=$1
 if [ -z $SSH_PORT ]; then
   SSH_PORT=2222
 fi
-ssh vagrant@localhost -p $SSH_PORT -t "sudo cp /vagrant/sudoers /etc/sudoers"
+echo "Enter password 'vagrant' when prompted:"
+ssh vagrant@localhost -p $SSH_PORT -t "sudo cp /vagrant/sudoers /etc/sudoers" && echo "Sudo access to your vagrant box has been restored."
